559a5e4334e6523fc019761865812b4dff510e6f
rcole831 <r_cole831@aol.com>
2017-10-15 20:05:08 -0400

    Deleted old file

--- backend-starter-code/models/step.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = (sequelize, DataTypes) => {
-	const Step = sequelize.define('Step', {
-		post_ID: DataTypes.STRING,
-		content: DataTypes.TEXT,
-		gif_location: DataTypes.STRING,
-		guide_ID: DataTypes.STRING,
-	});
-}
