1179cca1fbc35f7db4749930833e0b2aa742a21b
Irteza Syed <irteza.m.syed@gmail.com>
2017-12-01 14:02:23 -0500

    guide-posting hopefully works

--- guidesmiths-react/src/guide-list/GuideListPage.js
+++ guidesmiths-react/src/guide-list/GuideListPage.js
@@ -39,48 +39,7 @@ class GuideListPage extends Component {
     )
   }
 
-  {/*
-  render() {
-    return(
-      <div className="panel panel-body">
-        <label>List of Guides</label><br/><br/>
-        <label>Guide 1</label>
-          <div className="panel panel-body">
-            <ul>
-              <li>
-                <label>Step 1</label>
-                <p>Put instructions and GIF here</p>
-              </li>
-              <li>
-                <label>Step 2</label>
-                <p>Put instructions and GIF here</p>
-              </li>
-              <li>
-                <label>Step n</label>
-                <p>Put instructions and GIF here</p>
-              </li>
-            </ul>
-          </div>
-        <label>Guide 2</label>
-          <div className="panel panel-body">
-            <ul>
-              <li>
-                <label>Step 1</label>
-                <p>Put instructions and GIF here</p>
-              </li>
-              <li>
-                <label>Step 2</label>
-                <p>Put instructions and GIF here</p>
-              </li>
-              <li>
-                <label>Step n</label>
-                <p>Put instructions and GIF here</p>
-              </li>
-            </ul>
-          </div>
-      </div>
-    );
-  }*/}
+  
 }
 
 export default GuideListPage;
