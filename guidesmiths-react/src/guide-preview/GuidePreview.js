a09241a4032b2720003e3bc67cbb8d968d139926
Daniel Chan <dchan7199@gmail.com>
2017-12-13 13:31:15 -0500

    working on collapsible panels

--- guidesmiths-react/src/guide-preview/GuidePreview.js
+++ guidesmiths-react/src/guide-preview/GuidePreview.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import { Panel, Image} from 'react-bootstrap';
+import React from 'react';
+import { Button, Panel, Image, ListGroup, ListGroupItem} from 'react-bootstrap';
 import { Route } from 'react-router-dom';
-import { LinkContainer } from 'react-router-bootstrap';
 import GuideContentPage from '../guide-content-page/GuideContentPage.js';
 
 
@@ -9,14 +8,16 @@ const GuidePreview = (props) => {
   const guide = props.guide;
   const steps = [];
   for (let step in props.steps){
-    if(props.steps[step].GuideGuideID == props.guide.guideID)
+    if(props.steps[step].GuideGuideID === props.guide.guideID)
       steps.push(props.steps[step]);
   }
+  let open = props.open;
+  const index = props.index;
 
   let stepTitle = [];
   let stepContent = [];
   let stepGL = [];
-  if (steps.length != 0){
+  if (steps.length !== 0){
     console.log("STEPS", steps);
     for(let step in steps){
       stepTitle.push(<p>{steps[step].title}</p>)
@@ -26,44 +27,58 @@ const GuidePreview = (props) => {
   }
   //console.log("GUIDE", guide)
   console.log("GUIDE TITLE", guide)
+  console.log("OPEN = ", index, open, props.onClick)
    return (
-       <Panel>
-         <LinkContainer to={`/guide/${guide.guideID}`}><h2>{guide.title}</h2></LinkContainer>
+      <div className = "guidePanel">
+        <Panel header={guide.title} collapsible="true" expanded = {open} 
+            onSelect={props.onClick} eventKey={guide.guideID}>
+         <ListGroup fill>
+         <ListGroupItem bsStyle="info">  
+         <h2>{guide.title}</h2>
          <h3>{guide.subtitle}</h3>
          <Route path="`/guide/guide?={guide.guideID}`" component={GuideContentPage}/>
          <p>{`By ${guide.UserUserName}`}</p>
+         <h4>Summary:</h4> <p>{guide.summary}</p>
          <Image src={`http://localhost:8001/${guide.coverImageLocation}`} responsive/>
+        </ListGroupItem>  
+        <ListGroupItem bsStyle="success">   
          <h4>Step1:</h4> {stepTitle[0]}
          {stepGL[0]}
          <br></br>
          {stepContent[0]}
          <br></br>
+         </ListGroupItem>
+         <ListGroupItem bsStyle="info">
          <h4>Step2:</h4>{stepTitle[1]}
          {stepGL[1]}
          <br></br>
          {stepContent[1]}
          <br></br>
+         </ListGroupItem>
+         <ListGroupItem bsStyle="success">
          <h4>Step3:</h4>{stepTitle[2]}
          {stepGL[2]}
          <br></br>
          {stepContent[2]}
          <br></br>
+         </ListGroupItem>
+         <ListGroupItem bsStyle="info">
          <h4>Step4:</h4>{stepTitle[3]}
          {stepGL[3]}
          <br></br>
          {stepContent[3]}
          <br></br>
+         </ListGroupItem>
+         <ListGroupItem bsStyle="success">
          <h4>Step5:</h4>{stepTitle[4]}
          {stepGL[4]}
          <br></br>
          {stepContent[4]}
          <br></br>
-         {stepTitle[5]}
-         {stepGL[5]}
-         <br></br>
-         {stepContent[5]}
-         <h4>Summary:</h4> <p>{guide.summary}</p>
+         </ListGroupItem>
+      </ListGroup>
        </Panel>
+       </div>
    )
 }
 
